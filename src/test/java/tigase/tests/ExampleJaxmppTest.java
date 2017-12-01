bc438ef76b7412ad0e109fe8fedb50d4381a9d35
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2017-12-01 17:39:53 +0100

    #6275: updated licensing headers

--- src/test/java/tigase/tests/ExampleJaxmppTest.java
+++ src/test/java/tigase/tests/ExampleJaxmppTest.java
@@ -1,12 +1,12 @@
 /*
  * ExampleJaxmppTest.java
  *
- * Tigase Jabber/XMPP Server - TTS-NG
- * Copyright (C) 2004-2015 "Tigase, Inc." <office@tigase.com>
+ * Tigase TTS-NG
+ * Copyright (C) 2015-2017 "Tigase, Inc." <office@tigase.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
+ * the Free Software Foundation, version 3 of the License,
  * or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. Look for COPYING file in the top folder.
  * If not, see http://www.gnu.org/licenses/.
- *
  */
 package tigase.tests;
 
--- src/test/java/tigase/tests/archive/TestMessageArchivingMUC.java
+++ src/test/java/tigase/tests/archive/TestMessageArchivingMUC.java
@@ -1,10 +1,12 @@
 /*
- * Tigase Jabber/XMPP Server - TTS-NG
- * Copyright (C) 2004-2016 "Tigase, Inc." <office@tigase.com>
+ * TestMessageArchivingMUC.java
+ *
+ * Tigase TTS-NG
+ * Copyright (C) 2015-2017 "Tigase, Inc." <office@tigase.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
+ * the Free Software Foundation, version 3 of the License,
  * or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -15,7 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. Look for COPYING file in the top folder.
  * If not, see http://www.gnu.org/licenses/.
- *
  */
 package tigase.tests.archive;
 
