9b7e806183259edbd4c99866a9b7351ad3160c32
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2019-03-26 16:50:04 +0100

    #8765: readded test cases removed by accident

---
+++ src/test/java/tigase/tests/workgroup/AbstractWorkgroupTest.java
@@ -0,0 +1,40 @@
+/*
+ * AbstractAuditlogTest.java
+ *
+ * Tigase TTS-NG
+ * Copyright (C) 2015-2018 "Tigase, Inc." <office@tigase.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+package tigase.tests.workgroup;
+
+import org.testng.IHookable;
+import tigase.jaxmpp.core.client.JID;
+import tigase.tests.AbstractSkippableTest;
+
+public class AbstractWorkgroupTest
+		extends AbstractSkippableTest
+		implements IHookable {
+
+	@Override
+	protected JID getComponentJID() {
+		return JID.jidInstance("wg." + getDomain(0));
+	}
+
+	@Override
+	protected String getComponentName() {
+		return "WorkGroup";
+	}
+}
