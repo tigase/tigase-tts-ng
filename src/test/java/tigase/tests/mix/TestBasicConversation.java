8a776119b790e3afa5f1b4f54161d6e0eb45f36c
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2020-02-27 12:47:07 +0100

    Added test cases for changes in MAM rules #messagearchiving-58

--- src/test/java/tigase/tests/mix/TestBasicConversation.java
+++ src/test/java/tigase/tests/mix/TestBasicConversation.java
@@ -6,20 +6,28 @@ import org.testng.annotations.Test;
 import tigase.jaxmpp.core.client.Connector;
 import tigase.jaxmpp.core.client.JID;
 import tigase.jaxmpp.core.client.SessionObject;
+import tigase.jaxmpp.core.client.XMPPException;
+import tigase.jaxmpp.core.client.exceptions.JaxmppException;
 import tigase.jaxmpp.core.client.xml.Element;
 import tigase.jaxmpp.core.client.xml.ElementBuilder;
+import tigase.jaxmpp.core.client.xmpp.modules.mam.MessageArchiveManagementModule;
 import tigase.jaxmpp.core.client.xmpp.modules.pubsub.PubSubModule;
 import tigase.jaxmpp.core.client.xmpp.stanzas.IQ;
 import tigase.jaxmpp.core.client.xmpp.stanzas.Message;
 import tigase.jaxmpp.core.client.xmpp.stanzas.Stanza;
 import tigase.jaxmpp.core.client.xmpp.stanzas.StreamPacket;
+import tigase.jaxmpp.core.client.xmpp.utils.RSM;
 import tigase.jaxmpp.j2se.Jaxmpp;
+import tigase.jaxmpp.j2se.connectors.socket.JaxmppHostnameVerifier;
 import tigase.tests.AbstractTest;
 import tigase.tests.Mutex;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.security.cert.Certificate;
+import java.util.*;
+
+import static org.junit.Assert.assertTrue;
+import static tigase.jaxmpp.j2se.connectors.socket.SocketConnector.HOSTNAME_VERIFIER_DISABLED_KEY;
+import static tigase.jaxmpp.j2se.connectors.socket.SocketConnector.HOSTNAME_VERIFIER_KEY;
 
 public class TestBasicConversation
 		extends AbstractTest {
@@ -36,9 +44,31 @@ public class TestBasicConversation
 
 	@BeforeClass
 	public void setUp() throws Exception {
-		this.jaxmpp1 = createAccount().setLogPrefix("user1").build().createJaxmpp().setConnected(true).build();
-		this.jaxmpp2 = createAccount().setLogPrefix("user2").build().createJaxmpp().setConnected(true).build();
-		this.jaxmpp3 = createAccount().setLogPrefix("user3").build().createJaxmpp().setConnected(true).build();
+		JaxmppHostnameVerifier hostnameVerifier = new JaxmppHostnameVerifier() {
+			@Override
+			public boolean verify(String hostname, Certificate certificate) {
+				return true;
+			}
+		};
+
+
+		this.jaxmpp1 = createAccount().setLogPrefix("user1").build().createJaxmpp().setConfigurator(jaxmpp -> {
+			jaxmpp.getModulesManager().register(new MessageArchiveManagementModule());
+			jaxmpp.getSessionObject().setUserProperty(HOSTNAME_VERIFIER_DISABLED_KEY, true);
+			jaxmpp.getSessionObject().setUserProperty(HOSTNAME_VERIFIER_KEY, hostnameVerifier);
+			return jaxmpp;
+		}).setConnected(true).build();
+		this.jaxmpp2 = createAccount().setLogPrefix("user2").build().createJaxmpp().setConfigurator(jaxmpp -> {
+			jaxmpp.getSessionObject().setUserProperty(HOSTNAME_VERIFIER_KEY, hostnameVerifier);
+			jaxmpp.getSessionObject().setUserProperty(HOSTNAME_VERIFIER_DISABLED_KEY, true);
+//			jaxmpp.getConnectionConfiguration().setPort(5322);
+			return jaxmpp;
+		}).setConnected(true).build();
+		this.jaxmpp3 = createAccount().setLogPrefix("user3").build().createJaxmpp().setConfigurator(jaxmpp -> {
+			jaxmpp.getSessionObject().setUserProperty(HOSTNAME_VERIFIER_DISABLED_KEY, true);
+			jaxmpp.getSessionObject().setUserProperty(HOSTNAME_VERIFIER_KEY, hostnameVerifier);
+			return jaxmpp;
+		}).setConnected(true).build();
 
 		this.mixJID = TestDiscovery.findMIXComponent(jaxmpp1);
 
@@ -46,6 +76,32 @@ public class TestBasicConversation
 	}
 
 	@Test
+	public void enableMamForUser1() throws Exception {
+		final Mutex mutex = new Mutex();
+		jaxmpp1.getModulesManager().getModule(MessageArchiveManagementModule.class).updateSetttings(
+				MessageArchiveManagementModule.DefaultValue.always, null, null, new MessageArchiveManagementModule.SettingsCallback() {
+					@Override
+					public void onSuccess(MessageArchiveManagementModule.DefaultValue defValue, List<JID> always,
+										  List<JID> never) throws JaxmppException {
+						mutex.notify("mam:enable:success", "mam:enable");
+					}
+
+					@Override
+					public void onError(Stanza responseStanza, XMPPException.ErrorCondition error)
+							throws JaxmppException {
+						mutex.notify("mam:enable:error", "mam:enable");
+					}
+
+					@Override
+					public void onTimeout() throws JaxmppException {
+						mutex.notify("mam:enable:timeout", "mam:enable");
+					}
+				});
+		mutex.waitFor(10 * 1000, "mam:enable");
+		assertTrue(mutex.isItemNotified("mam:enable:success"));
+	}
+
+	@Test(dependsOnMethods = "enableMamForUser1")
 	public void testCreateChannel() throws Exception {
 		ElementBuilder request = ElementBuilder.create("iq")
 				.setAttribute("id", "create-01")
@@ -256,6 +312,66 @@ public class TestBasicConversation
 			jaxmpp2.getEventBus().remove(handler);
 			jaxmpp3.getEventBus().remove(handler);
 		}
+	}
+
+	@Test(dependsOnMethods = {"testJoinUser2"})
+	public void testUser2JoinInUser1MAM() throws Exception {
+		final Mutex mutex = new Mutex();
+
+		MessageArchiveManagementModule.MessageArchiveItemReceivedEventHandler handler = (sessionObject, queryid, messageId, timestamp, message) -> {
+			final Element event = message.getChildrenNS("event", "http://jabber.org/protocol/pubsub#event");
+			List<Element> tmp = event == null ? null : event.getChildren("items");
+			final Element items = tmp == null || tmp.isEmpty() ? null : tmp.get(0);
+			final String nodeName = items == null ? null : items.getAttribute("node");
+
+			if (items != null && items.getChildren() != null) {
+				for (Element item : items.getChildren()) {
+					final String type = item.getName();
+					final String itemId = item.getAttribute("id");
+//					final Element payload = item.getFirstChild();
+					mutex.notify("mam:received:" + message.getFrom() + ":" + type + ":" + itemId);
+				}
+			}
+		};
+		jaxmpp1.getContext().getEventBus().addHandler(
+				MessageArchiveManagementModule.MessageArchiveItemReceivedEventHandler.MessageArchiveItemReceivedEvent.class, handler);
+
+		MessageArchiveManagementModule.Query query = new MessageArchiveManagementModule.Query();
+		query.setStart(new Date(System.currentTimeMillis() - (60*60*1000)));
+		query.setWith(JID.jidInstance(channelName, mixJID.getDomain()));
+		String queryId = UUID.randomUUID().toString();
+	 	jaxmpp1.getModulesManager().getModule(MessageArchiveManagementModule.class).queryItems(query, queryId, new RSM(100), new MessageArchiveManagementModule.ResultCallback() {
+
+			@Override
+			public void onError(Stanza responseStanza, XMPPException.ErrorCondition error) throws JaxmppException {
+				mutex.notify("mam:participants:query:error", "mam:participants:query");
+			}
+
+			@Override
+			public void onTimeout() throws JaxmppException {
+				mutex.notify("mam:participants:query:timeout", "mam:participants:query");
+			}
+
+			@Override
+			public void onSuccess(String queryid, boolean complete, RSM rsm) throws JaxmppException {
+				mutex.notify("mam:participants:query:success", "mam:participants:query");
+			}
+		});
+
+	 	mutex.waitFor(10 * 1000, "mam:participants:query");
+	 	assertTrue(mutex.isItemNotified("mam:participants:query:success"));
+
+	 	String expItemid = null;
+	 	for (Map.Entry<String,String> e :  participants.entrySet()) {
+	 		if (e.getValue().equals("user2")) {
+	 			expItemid = e.getKey();
+	 			break;
+			}
+		}
+	 	Thread.sleep(100);
+	 	assertTrue(mutex.isItemNotified("mam:received:" + channelName + "@" + mixJID.getDomain() + ":item:" + expItemid));
+
+	 	jaxmpp1.getContext().getEventBus().remove(handler);
 	}
 
 	@Test(dependsOnMethods = {"testJoinUser3"})
